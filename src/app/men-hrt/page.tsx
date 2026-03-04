051bf8145fa84f0967c37d7c1215dd8d879f941f
ILhankhondaker <ilhankhondaker@gmail.com>
2026-03-04 13:01:46 +0600

    add faq

--- src/app/men-hrt/page.tsx
+++ src/app/men-hrt/page.tsx
@@ -1,8 +1,12 @@
+import { Faq } from '@/components/Faq'
 import MernhrtHero from '@/components/MenhrtHero'
 import React from 'react'
 
 export default function page() {
   return (
+<>
     <MernhrtHero/>
+    <Faq/>
+</>
   )
 }
